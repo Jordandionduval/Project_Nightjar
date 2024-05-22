27dc5a2b5b6a69fd2b6a5c0ed86b51d0f5d4ecc0
Jordan Dion-Duval <jordan.dion.duval@outlook.com>
2024-05-21 23:54:25 -0400

    Added BP Project

---
+++ Projecct_NightjarBP/Source/Projecct_NightjarBP/Projecct_NightjarBP.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Projecct_NightjarBP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Projecct_NightjarBP, "Projecct_NightjarBP" );
