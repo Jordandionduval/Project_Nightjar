e83f5efd0347c1bf65ac06cd6d24810dfd50b352
Jordan Dion-Duval <theravingdead05@gmail.com>
2024-05-21 21:08:50 -0400

    Delete the project

--- Project_Nightjar/Intermediate/Build/Win64/x64/Project_NightjarEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/joe-b/Documents/0 Projects/GameJam/Project_Nightjar/Project_Nightjar/Intermediate/Build/Win64/x64/Project_NightjarEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- Project_Nightjar/Intermediate/Build/Win64/x64/Project_NightjarEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.38.33135
-#include "C:/Users/joe-b/Documents/0 Projects/GameJam/Project_Nightjar/Project_Nightjar/Intermediate/Build/Win64/x64/Project_NightjarEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h"
