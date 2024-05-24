af12633155383e316d92bcadf8baeaa02f059882
Diego <diegoduran2001@gmail.com>
2024-05-24 13:35:22 -0400

    script character controller

---
+++ Projecct_NightjarBP/Source/Projecct_NightjarBP/Public/CharacterController.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CharacterController.generated.h"
+#include "Camera/CameraComponent.h"
+
+UCLASS()
+class PROJECCT_NIGHTJARBP_API ACharacterController : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACharacterController();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	//Actions
+	void MoveRight(float value);
+	void MoveUp(float value);
+	void MeleeAttack();
+	void GrabEnemy();
+	void ThrowEnemy();
+	void Dash();
+	void Jump();
+	void StopJumping();
+
+
+
+	//Health Management
+	void TakeDamage(float damageAmount);
+	void Die();
+
+	//Propreties
+	bool isGrabbingEnemy;
+	AActor* GrabbedEnemy;
+	float Health;
+	float maxHealth;
+	float DashDistance;
+	float DashCoolDown;
+	bool bCanDash;
+
+	//Timers
+	FTimerHandle DashCooldownTimerHandle;
+	
+	//Helper functions
+	void ResetDash();
+
+
+	//Camera component 
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		UCameraComponent* TopDownCameraComponent;
+};
